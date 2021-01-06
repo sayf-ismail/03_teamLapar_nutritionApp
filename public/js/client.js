02eb6d7a12810c4826b4db7057da92130dba9da0
Sayf Ismail <sayfti@gmail.com>
2021-01-06 17:24:51 +0800

    search results now working. Need to format and fix layout.

--- public/js/client.js
+++ public/js/client.js
@@ -13,3 +13,35 @@ function autoSlides () {
     console.log(current);
 }
 autoSlides();
+
+function getFoodNutrition(){
+    var searchText = document.querySelector('#searchtext')
+    axios
+        .get('/api/food_db', {
+            params: {
+                searchText: searchText.value
+            }
+        })
+        .then(res => res.data.forEach(food => {
+            console.log(res.data)
+            var foodNutrition = document.querySelector('#results') 
+            foodNutrition.innerHTML += `
+            <div class = "className"> 
+                <img src = '${food.picture_url}'>
+                <h3>Food Nutrition Details</h3>
+                <p>Dish: '${food.dish}' </p>
+                <p>Portion: '${food.portion}'</p>
+                <p>Calories:'${food.calories}'</p>
+                <p>Carbs: '${food.carbs}'</p>
+                <p>Fat: '${food.fat}'</p>
+                <p>Protein: '${food.protein}'</p>
+            </div>
+            `
+        }));
+}
+
+var searchForm = document.querySelector('#searchForm')
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    getFoodNutrition()
+})

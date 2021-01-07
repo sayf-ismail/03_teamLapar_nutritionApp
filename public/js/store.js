ea20552daa267db3b8ba7c4d50bc0a53c57847f1
Sayf Ismail <sayfti@gmail.com>
2021-01-07 15:58:52 +0800

    Working on Difference tag.

--- public/js/store.js
+++ public/js/store.js
@@ -22,10 +22,10 @@ function addItemToCart(dishName, calories, imageSrc) {
         <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
         <span class="cart-item-title">${dishName}</span>
     </div>
-    <span class="cart-price cart-column">${calories}</span>
+    <span class="cart-calorie cart-column">${calories}</span>
     <div class="cart-quantity cart-column">
         <input class="cart-quantity-input" type="number" value="1">
-        <button class="btn btn-danger" type="button">REMOVE</button>
+        <button class="btn btn-danger" type="button" onClick="removeCartItem(event)">REMOVE</button>
     </div>`
 
   cartRow.innerHTML = cartRowContents
@@ -33,5 +33,48 @@ function addItemToCart(dishName, calories, imageSrc) {
 }
 
 function updateCartTotal() {
-
+  var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+  var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+  var total = 0
+  for (var i = 0; i < cartRows.length; i++) {
+      var cartRow = cartRows[i]
+      var calorieElement = cartRow.getElementsByClassName('cart-calorie')[0]
+      var calorie = parseFloat(calorieElement.innerText.replace('$', ''))
+      total = total + calorie
+  }
+  var totalTag = document.querySelector('.total-calories')
+  var h3Tag = document.createElement('h3')
+  h3Tag.textContent = `${total}`
+  totalTag.append(h3Tag)
+  console.log(total)
+  var targetValue = Number(document.querySelector('.user-target-value').value)
+  updateDifference(total,targetValue)
 }
+
+function updateDifference(total, targetValue) {
+  var difference = targetValue - total
+  var differenceTag = document.querySelector('.difference')
+  var h3Tag = document.createElement('h3')
+  h3Tag.textContent = `${difference}`
+  differenceTag.append(h3Tag)
+  console.log(difference)
+  if (target > total) {
+    console.log('Congrats! You can eat more!')
+  } else {
+    console.log('STOP EATING! TOO MANY CALORIES! THINK OF YOUR HEALTH! GO EXERCISE!')
+  }
+  
+}
+
+function removeCartItem(event) {
+  console.log("remove clicked!")
+  var buttonClicked = event.target
+  buttonClicked.parentElement.parentElement.remove()
+  updateCartTotal()
+}
+
+var totalBtn = document.querySelector('.totalBtn')
+totalBtn.addEventListener('click', function(event){
+    event.preventDefault()
+    updateCartTotal()
+})
